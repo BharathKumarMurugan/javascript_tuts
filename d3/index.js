eb75446a7a5278645822505f472cc8d8341aedba
Bharath Kumar <bharathkumarmurugan2016@gmail.com>
2021-03-15 21:49:41 +0530

    Update index.js

--- d3/index.js
+++ d3/index.js
@@ -325,7 +325,7 @@ svg
 /**
  * Add a Tooltip to a D3 Element
  */
-const dataset = [12, 31, 22, 17, 25, 18, 29, 14, 9];
+/*const dataset = [12, 31, 22, 17, 25, 18, 29, 14, 9];
 const w = 500,
   h = 100;
 const svg = d3.select("body").append("svg").attr("width", w).attr("height", h);
@@ -353,3 +353,96 @@ svg
   .text((d, i) => d)
   .style("font-size", "12px")
   .attr("fill", "red");
+*/
+
+/**
+ * Create a Scatterplot with SVG Circles
+ */
+/*
+const dataset = [
+    [34, 78],
+    [109, 280],
+    [310, 120],
+    [79, 411],
+    [420, 220],
+    [233, 145],
+    [333, 96],
+    [222, 333],
+    [78, 320],
+    [21, 123],
+];
+
+const w = 500,
+    h = 500;
+
+const svg = d3.select("body").append("svg").attr("width", w).attr("height", h);
+
+svg.selectAll("circle").data(dataset).enter().append("circle");
+*/
+
+/**
+ * Add Attributes to the Circle Elements
+ */
+/*const dataset = [
+    [34, 78],
+    [109, 280],
+    [310, 120],
+    [79, 411],
+    [420, 220],
+    [233, 145],
+    [333, 96],
+    [222, 333],
+    [78, 320],
+    [21, 123],
+];
+
+const w = 500,
+    h = 500;
+
+const svg = d3.select("body").append("svg").attr("width", w).attr("height", h);
+
+svg.selectAll("circle")
+    .data(dataset)
+    .enter()
+    .append("circle")
+    .attr("cx", (d, i) => d[0])
+    .attr("cy", (d, i) => h - d[1])
+    .attr("r", 5);
+*/
+
+/**
+ * Add Labels to Scatter Plot Circles
+ */
+const dataset = [
+    [34, 78],
+    [109, 280],
+    [310, 120],
+    [79, 411],
+    [420, 220],
+    [233, 145],
+    [333, 96],
+    [222, 333],
+    [78, 320],
+    [21, 123],
+];
+
+const w = 500,
+    h = 500;
+
+const svg = d3.select("body").append("svg").attr("width", w).attr("height", h);
+
+svg.selectAll("circle")
+    .data(dataset)
+    .enter()
+    .append("circle")
+    .attr("cx", (d, i) => d[0])
+    .attr("cy", (d, i) => h - d[1])
+    .attr("r", 5);
+
+svg.selectAll("text")
+    .data(dataset)
+    .enter()
+    .append("text")
+    .attr("x", (d, i) => d[0] + 5)
+    .attr("y", (d, i) => h - d[1])
+    .text((d) => d[0] + ", " + d[1]);
